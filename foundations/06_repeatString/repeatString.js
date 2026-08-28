c67820517b664ccd73d1be22c57630da07a811b1
annielkn <61274383+annielkn@users.noreply.github.com>
2026-08-28 20:56:55 +0100

    solved js

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, n) {
+    let out = "";
+    for (var i = 0; i < n; i++){
+        out += str;
+    }
+    return out;
 };
 
 // Do not edit below this line
