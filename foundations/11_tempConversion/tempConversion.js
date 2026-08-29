0e06e45ac618c8aff0af9d68c086127be5bd04d9
annielkn <61274383+annielkn@users.noreply.github.com>
2026-08-29 14:18:47 +0100

    solved

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(deg) {
+  return Math.round((deg-32)*50/9)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(deg) {
+  return Math.round((deg*1.8+32)*10)/10
 };
 
 // Do not edit below this line
