22834e3b3e8282c35d2083eb4cb688533522de26
annielkn <61274383+annielkn@users.noreply.github.com>
2026-08-29 14:03:52 +0100

    solved

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    const triangle = n => n*(n+1)/2;
+    if (Number.isInteger(a) && Number.isInteger(b) && a > 0 && b > 0){
+        return triangle(Math.max(a,b))-triangle(Math.min(a,b)-1); 
+    }
+    return `ERROR`;
 };
 
 // Do not edit below this line
