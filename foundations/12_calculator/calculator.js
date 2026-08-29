d56705180646b1b9ebb4e228f31d6352201bcea9
annielkn <61274383+annielkn@users.noreply.github.com>
2026-08-29 14:35:58 +0100

    solved

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((t, curr) => (t + curr), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((t, curr) => (t * curr), 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  out = 1;
+	for (i = 1; i <= n; i++){
+    out = out * i; 
+  }
+  return out;
 };
 
 // Do not edit below this line
