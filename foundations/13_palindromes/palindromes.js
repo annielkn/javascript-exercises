c0cfd32f7c2dc992795ebfd8e9ec5d41cde3e338
annielkn <61274383+annielkn@users.noreply.github.com>
2026-08-30 18:38:56 +0100

    solved without memoisation

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,4 +1,25 @@
-const palindromes = function () {
+const palindromes = function (s) {
+    let isAlnum = str => /^[a-zA-Z0-9]$/.test(str);
+    
+    let l = 0, r = s.length - 1;
+
+    while (l < r){
+        if (!isAlnum(s[l])) {
+            l += 1;
+            continue;
+        }
+        if (!isAlnum(s[r])) {
+            r -= 1;
+            continue;
+        }
+        if (s[l].toLowerCase() != s[r].toLowerCase()){
+            return false;
+        }
+        l++;
+        r--;
+    }
+    
+    return true;
 
 };
 
