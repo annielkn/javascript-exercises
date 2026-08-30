b2d573b67a078ec52c6d110a135c303bfb8f01b2
annielkn <61274383+annielkn@users.noreply.github.com>
2026-08-30 19:03:41 +0100

    solved

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.sort(
+        function (a, b) {
+            const yr = new Date().getFullYear();
+            const agea = (a.yearOfDeath || yr) - a.yearOfBirth
+            const ageb = (b.yearOfDeath || yr) - b.yearOfBirth
+            return agea > ageb ? -1 : 1;
+            }
+        )[0];
 };
 
 // Do not edit below this line
